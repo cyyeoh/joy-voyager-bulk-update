a0dc66f1462e73b1a2babd4dca6e21f05e97258b
Chun Yuan  Yeoh <cyyeoh@techtants.com>
2024-02-19 15:12:23 +0800

    6

--- src/VoyagerBulkUpdateServiceProvider.php
+++ src/VoyagerBulkUpdateServiceProvider.php
@@ -30,7 +30,7 @@ class VoyagerBulkUpdateServiceProvider extends ServiceProvider
         Voyager::addAction(\Joy\VoyagerBulkUpdate\Actions\BulkUpdateAction::class);
         Voyager::addAction(\Joy\VoyagerBulkUpdate\Actions\BulkAssigneeUpdateAction::class);
         Voyager::addAction(\Joy\VoyagerBulkUpdate\Actions\BulkParentUpdateAction::class);
-        Voyager::addAction(\Joy\VoyagerBulkUpdate\Actions\BulkStatusUpdateAction::class);
+        // Voyager::addAction(\Joy\VoyagerBulkUpdate\Actions\BulkStatusUpdateAction::class);
         Voyager::addAction(\Joy\VoyagerBulkUpdate\Actions\BulkRoleUpdateAction::class);
         Voyager::addAction(\Joy\VoyagerBulkUpdate\Actions\BulkFeaturedUpdateAction::class);
 
